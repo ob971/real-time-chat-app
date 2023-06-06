2644eedb9fa6ad99b278c1aa3d5e4196b48de1f8
obsa <obsukebede2022@gmail.com>
2023-06-06 12:00:00 +0300

    upadting

--- backend/middleware/protectRouter.js
+++ backend/middleware/protectRouter.js
@@ -1,5 +1,5 @@
-// import jwt from "jsonwebtoken";
-// import User from "../models/user.model.js";
+ import jwt from "jsonwebtoken";
+ import User from "../models/user.model.js";
 
 const protectRoute = async (req, res, next) => {
 	try {
