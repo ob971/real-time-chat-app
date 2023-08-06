1debda010884965eb2adc8cde11edb909af2afa4
obsa <obsukebede2022@gmail.com>
2023-08-06 12:00:00 +0300

    upadting

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 const mongoURI = 'mongodb://localhost:5001/real time chat app';
 
+
+
 const connectToMongoDB = async () => {
 	try {
 		await mongoose.connect(process.env.MONGO_DB_URI);
