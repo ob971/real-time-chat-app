35889cc11e392b0f3f20f5fcb020098b34503d1f
obsa <obsukebede2022@gmail.com>
2024-02-22 00:18:13 +0300

    progress

---
+++ backend/routes/auth.routes.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/login", (req, res) => {
+  res.send("Login Route");
+});
+
+
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,17 @@
-const express = require('express');
-
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
+dotenv.config();
+const PORT = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+  // root route http://localhost:5000/
+  res.send("Hello World!");
+});
 
-app.listen(5000, () => console.log('Server is running on port 5000'));
+// Corrected route declaration
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
