37db7ef9338f9593c9f7f223c6fd60fbc0f4a50d
obsa <obsukebede2022@gmail.com>
2024-02-22 20:50:47 +0300

    updating

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
+
 import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+
 
 const app = express();
 dotenv.config();
-const PORT = process.env.PORT || 5000;
+app.use(express.json());//to parse the incoming requests with JSON payloads(from req.)
+
+const PORT = process.env.PORT || 5001;
 
 app.get("/", (req, res) => {
-  // root route http://localhost:5000/
+  // root route http://localhost:5001/
   res.send("Hello World!");
 });
 
 // Corrected route declaration
 app.use("/api/auth", authRoutes);
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, () =>{
+  connectToMongoDB();
+
+
+console.log(`Server is running on port ${PORT}`)
+});
