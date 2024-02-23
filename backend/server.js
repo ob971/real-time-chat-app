ae5c24a711cb52de8aef8c5e02e0c80fd63f1ae7
obsa <obsukebede2022@gmail.com>
2024-02-23 16:47:46 +0300

    updating push

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,36 @@
 import path from "path";
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 
 
 const app = express();
+
+const __dirname = path.resolve();
+
 dotenv.config();
-app.use(express.json());//to parse the incoming requests with JSON payloads(from req.)
 
-const PORT = process.env.PORT || 5001;
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  // root route http://localhost:5001/
-  res.send("Hello World!");
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
-// Corrected route declaration
-app.use("/api/auth", authRoutes);
-
-app.listen(PORT, () =>{
-  connectToMongoDB();
-
-
-console.log(`Server is running on port ${PORT}`)
+server.listen(PORT, () => {
+	connectToMongoDB();
+	console.log(`Server Running on port ${PORT}`);
 });
